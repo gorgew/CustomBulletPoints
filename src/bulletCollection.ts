4dbd174fd92a00c92fc7a3fb6b7692722162857b
George Wang <ge0rge@seas.upenn.edu>
2021-07-26 11:16:53 -0400

    Refresh gitignore

--- src/bulletCollection.ts
+++
@@ -1,9 +0,0 @@
-import * as vscode from 'vscode';
-
-export interface BulletCollection extends vscode.QuickPickItem{
-
-    label:string;
-    stringSize:number;
-    bulletStringArray:string[];
-    detail:string;
-}
